135c94355ace2835982f01adfca294c6828e228f
lishiyong <lishiyong1@gmail.com>
2012-04-07 22:39:28 +0800

    g

---
+++ Sources/YCLib/YCGFunctions.h
@@ -0,0 +1,14 @@
+//
+//  YCGFunctions.h
+//  iAlarm
+//
+//  Created by li shiyong on 12-4-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef iAlarm_YCGFunctions_h
+#define iAlarm_YCGFunctions_h
+
+
+
+#endif
---
+++ Sources/YCLib/YCGFunctions.m
@@ -0,0 +1,9 @@
+//
+//  YCGFunctions.c
+//  iAlarm
+//
+//  Created by li shiyong on 12-4-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
