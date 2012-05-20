f33bf1e7148519060cad67a1c43960e5dc01e320
lishiyong <lishiyong1@gmail.com>
2012-05-21 00:21:18 +0800

    dd

---
+++ YCLib/YCCategories/UIViewController+YC.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+YC.h
+//  iAlarm
+//
+//  Created by li shiyong on 12-5-20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (YC)
+
+@end
