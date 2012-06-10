6bcbda1cd39296f53e8d0ba7d4c78f65504d485a
lishiyong <lishiyong1@gmail.com>
2012-06-10 23:16:21 +0800

    u

---
+++ YCLib/YCCollections/YCPair.h
@@ -0,0 +1,13 @@
+//
+//  YCPair.h
+//  iAlarm
+//
+//  Created by li shiyong on 12-6-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YCPair : NSObject
+
+@end
---
+++ YCLib/YCCollections/YCPair.m
@@ -0,0 +1,13 @@
+//
+//  YCPair.m
+//  iAlarm
+//
+//  Created by li shiyong on 12-6-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "YCPair.h"
+
+@implementation YCPair
+
+@end
