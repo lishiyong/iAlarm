5b56ab7cef7a8525f5eab31102e6d6ed28d1a4f4
lishiyong <lishiyong1@gmail.com>
2012-07-31 14:23:55 +0800

    d

---
+++ Sources/newAbout/LocalizedStringAbout.h
@@ -0,0 +1,14 @@
+//
+//  LocalizedStringAbout.h
+//  iAlarm
+//
+//  Created by li shiyong on 12-7-30.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef iAlarm_LocalizedStringAbout_h
+#define iAlarm_LocalizedStringAbout_h
+
+
+
+#endif
